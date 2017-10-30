3e216d2e45c35903063d5249526cf003c46222d8
AF <sing.adrian69@gmail.com>
2017-10-30 13:38:19 +0800

    Change in parameters of block, transaction and chain

--- main.py
+++ main.py
@@ -30,12 +30,13 @@ def main():
             txnList.append(makeTxn(txn['_owner'], txn['_recipient'], txn['content']))
             print 'Done Receiving\n'
             newBlock = makeBlock(txnList, chain)
-            checkBlockValidity(newBlock, chain[-1])
-            chain.append(newBlock)
-            checkChain(chain)
-            viewChain(chain)
-            print 'Writing to file...\n'
-            writeChain(chain, blockLocation)
+            if (checkBlockValidity(newBlock, chain[-1])):
+                txnList.pop()
+                chain.append(newBlock)
+                if (checkChain(chain)):
+                    viewChain(chain)
+                    print 'Writing to file...\n'
+                    writeChain(chain, blockLocation)
 
 if __name__ == '__main__':
     main()
