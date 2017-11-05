d86e1c80dfa0485607b7911681bc4a65e2b2f731
AF <sing.adrian69@gmail.com>
2017-11-06 02:00:56 +0800

    Server.py peer.py working p2p no broadcast yet

--- Sample Api/Client Nodes/txnForm.py
+++ Sample Api/Client Nodes/txnForm.py
@@ -2,8 +2,10 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, TextAreaField
 
 class SendTxnForm(FlaskForm):
+	peerAddr = StringField('Peer Address + Port')
 	senderKey = PasswordField('Sender Private Key')
 	receiverKey = StringField('Receiver Public Key')
 	txn = TextAreaField('Transaction')
 	submit = SubmitField("Send")
+	get = SubmitField("Get")
 
