af11f4ffb5a499cb482e74769973c4b16b814f79
Edrich Hans Chua <edrichhans@yahoo.com>
2018-04-20 12:42:19 +0800

    Added logs to login.py

--- login.py
+++ login.py
@@ -1,9 +1,5 @@
-import getpass
-import hashlib
-import random
-import sys
-import uuid
-import psycopg2
+import getpass, hashlib, random, sys, uuid, psycopg2
+from blockpy_logging import logger
 
 def ask_for_username():
     while True:
@@ -37,6 +33,8 @@ def store_info(conn, cur, privilege = None):
             VALUES(%s, %s, %s, %s);'''
         cur.execute(insertSql,(username, hashed_pass, salt, privilege))
         conn.commit()
+        logger.info('User Created',
+            extra={'username':username, 'privilege': privilege})
     except psycopg2.ProgrammingError as error:
         print error
 
@@ -54,18 +52,26 @@ def find_hashed_password_by_user(username, password, conn, cur, privilege = None
     # print elements
     if not user:
         print "Incorrect User/Password."
+        logger.warn('Invalid Login'
+            extra={'username':username, 'privilege':privilege})
+        return False
     else:
         if user[0] == username:
             hashed_password = hashlib.sha256(user[2].strip() + password).hexdigest()
+            logger.info('Login successful',
+                extra={'username':username, 'privilege':privilege})
             return user[1] == hashed_password
         else:
             print "Incorrect User/Password."
+            logger.warn('Invalid Login'
+                extra={'username':username, 'privilege':privilege})
             return False
 
 def checkIfUsersExist(conn, cur):
     createTableSql = 'CREATE TABLE IF NOT EXISTS users(username TEXT PRIMARY KEY, password_hash TEXT NOT NULL, salt TEXT NOT NULL, privilege INTEGER NOT NULL);'
     cur.execute(createTableSql)
     conn.commit()
+    logger.info('Users table created')
 
 def checkIfAdminExist(conn, cur):
     try:
@@ -78,6 +84,7 @@ def checkIfAdminExist(conn, cur):
     try:
         if not user:
             print "Admin does not exist, please enter desired credentials for admin:"
+            logger.info('Admin does not exist!')
             store_info(conn, cur, 0)
     except Exception as e:
         print e
@@ -87,6 +94,7 @@ def dropUsers(conn, cur):
     deleteTableSql = 'DROP TABLE IF EXISTS users;'
     cur.execute(deleteTableSql)
     conn.commit()
+    logger.info('Table users dropped')
 
 
 # if __name__ == "__main__":
