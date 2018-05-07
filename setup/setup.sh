2834de44cdfc386c4909045e73fd92b69d2459f5
jdlives <jlaborada.97@gmail.com>
2018-05-07 13:15:09 +0800

    Update setup.sh

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,16 +1,14 @@
-sudo apt-get update
-sudo apt-get install postgresql postgresql-contrib
-sudo pip install virtualenv
+sudo -H pip install virtualenv
 virtualenv --version
-virtualenv ../venv
+sudo -H virtualenv ../venv
 source ../venv/bin/activate
-sudo pip install -r requirements-pip.txt
-sudo -u postgres psql postgres -t "SELECT 1 FROM blockpy WHERE datname='blockpy'" | grep -q 1 || sudo -u postgres psql postgres -c "CREATE DATABASE blockpy"
-cat > ../database.ini <<- "EOF"
+sudo -H pip install -r requirements-pip.txt
+sudo bash -c 'cat > ../database.ini <<- "EOF"
 [postgresql]
 host=localhost
 database=blockpy
 user=postgres
 password=toor
 port=5432
-EOF
+EOF'
+
