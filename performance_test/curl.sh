4c770c6474ddda18aa40fe49e2711414df0385fa
jlaborada.97@gmail.com <jlaborada.97@gmail.com>
2018-05-15 16:30:46 +0800

    changed 10002 to 5002

--- performance_test/curl.sh
+++ performance_test/curl.sh
@@ -1,30 +1,30 @@
 sleep $((RANDOM %10))
-curl http://localhost:10002/insert -d 'content=a' -v --retry 10
+curl http://localhost:5002/insert -d 'content=a' -v --retry 10
 sleep $((RANDOM %10))
-curl http://localhost:10002/insert -d 'content=b' -v --retry 10
+curl http://localhost:5002/insert -d 'content=b' -v --retry 10
 sleep $((RANDOM %10))
-curl http://localhost:10002/insert -d 'content=c' -v --retry 10
+curl http://localhost:5002/insert -d 'content=c' -v --retry 10
 sleep $((RANDOM %10))
-curl http://localhost:10002/insert -d 'content=d' -v --retry 10
+curl http://localhost:5002/insert -d 'content=d' -v --retry 10
 sleep $((RANDOM %10))
-curl http://localhost:10002/insert -d 'content=e' -v --retry 10
+curl http://localhost:5002/insert -d 'content=e' -v --retry 10
 sleep $((RANDOM %10))
-curl http://localhost:10002/insert -d 'content=f' -v --retry 10
+curl http://localhost:5002/insert -d 'content=f' -v --retry 10
 sleep $((RANDOM %10))
-curl http://localhost:10002/insert -d 'content=g' -v --retry 10
+curl http://localhost:5002/insert -d 'content=g' -v --retry 10
 sleep $((RANDOM %10))
-curl http://localhost:10002/insert -d 'content=h' -v --retry 10
+curl http://localhost:5002/insert -d 'content=h' -v --retry 10
 sleep $((RANDOM %10))
-curl http://localhost:10002/insert -d 'content=i' -v --retry 10
+curl http://localhost:5002/insert -d 'content=i' -v --retry 10
 sleep $((RANDOM %10))
-curl http://localhost:10002/insert -d 'content=j' -v --retry 10
+curl http://localhost:5002/insert -d 'content=j' -v --retry 10
 sleep $((RANDOM %10))
-curl http://localhost:10002/insert -d 'content=k' -v --retry 10
+curl http://localhost:5002/insert -d 'content=k' -v --retry 10
 sleep $((RANDOM %10))
-curl http://localhost:10002/insert -d 'content=l' -v --retry 10
+curl http://localhost:5002/insert -d 'content=l' -v --retry 10
 sleep $((RANDOM %10))
-curl http://localhost:10002/insert -d 'content=m' -v --retry 10
+curl http://localhost:5002/insert -d 'content=m' -v --retry 10
 sleep $((RANDOM %10))
-curl http://localhost:10002/insert -d 'content=n' -v --retry 10
+curl http://localhost:5002/insert -d 'content=n' -v --retry 10
 sleep $((RANDOM %10))
-curl http://localhost:10002/insert -d 'content=o' -v --retry 10
+curl http://localhost:5002/insert -d 'content=o' -v --retry 10
