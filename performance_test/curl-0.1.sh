886bce4b6f0c2717b8467d42b7b957dd175e9c21
jlaborada.97@gmail.com <jlaborada.97@gmail.com>
2018-05-17 19:38:46 +0800

    Make curl 5 txns for testing

--- performance_test/curl-0.1.sh
+++ performance_test/curl-0.1.sh
@@ -8,13 +8,3 @@ sleep 0.1s
 curl http://localhost:5002/insert -d 'content=d' -v --retry 10
 sleep 0.1s
 curl http://localhost:5002/insert -d 'content=e' -v --retry 10
-sleep 0.1s
-curl http://localhost:5002/insert -d 'content=f' -v --retry 10
-sleep 0.1s
-curl http://localhost:5002/insert -d 'content=g' -v --retry 10
-sleep 0.1s
-curl http://localhost:5002/insert -d 'content=h' -v --retry 10
-sleep 0.1s
-curl http://localhost:5002/insert -d 'content=i' -v --retry 10
-sleep 0.1s
-curl http://localhost:5002/insert -d 'content=j' -v --retry 10
