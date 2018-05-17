d152d6d6cf68dba5191fd6bb32baab380fac0a4f
jlaborada.97@gmail.com <jlaborada.97@gmail.com>
2018-05-17 21:32:45 +0800

    Make random 5 txns

--- performance_test/curl-random.sh
+++ performance_test/curl-random.sh
@@ -8,13 +8,3 @@ sleep $((RANDOM %10))
 curl http://localhost:5002/insert -d 'content=d' -v --retry 10
 sleep $((RANDOM %10))
 curl http://localhost:5002/insert -d 'content=e' -v --retry 10
-sleep $((RANDOM %10))
-curl http://localhost:5002/insert -d 'content=f' -v --retry 10
-sleep $((RANDOM %10))
-curl http://localhost:5002/insert -d 'content=g' -v --retry 10
-sleep $((RANDOM %10))
-curl http://localhost:5002/insert -d 'content=h' -v --retry 10
-sleep $((RANDOM %10))
-curl http://localhost:5002/insert -d 'content=i' -v --retry 10
-sleep $((RANDOM %10))
-curl http://localhost:5002/insert -d 'content=j' -v --retry 10
